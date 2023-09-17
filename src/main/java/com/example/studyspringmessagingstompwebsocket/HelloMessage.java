6fea4035cd61ff7629c348ce690962b4deb6b5a7
Jaewook Choi <eari100@naver.com>
2023-09-17 23:24:23 +0900

    feat: HelloMessage 객체 추가

---
+++ src/main/java/com/example/studyspringmessagingstompwebsocket/HelloMessage.java
@@ -0,0 +1,16 @@
+package com.example.studyspringmessagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+
+    private String name;
+
+}
