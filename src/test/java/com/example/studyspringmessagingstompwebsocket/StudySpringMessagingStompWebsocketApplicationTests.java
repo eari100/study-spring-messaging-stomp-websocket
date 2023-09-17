9f4c2a355ea3f2b1394f039aa36db3b0a9235991
Jaewook Choi <eari100@naver.com>
2023-09-17 23:16:55 +0900

    Initial commit

---
+++ src/test/java/com/example/studyspringmessagingstompwebsocket/StudySpringMessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspringmessagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringMessagingStompWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
