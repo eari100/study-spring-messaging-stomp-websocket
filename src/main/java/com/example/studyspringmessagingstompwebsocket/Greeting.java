420f8f85b6dc2e9ccf434e0935960d119479c562
Jaewook Choi <eari100@naver.com>
2023-09-17 23:25:20 +0900

    feat: Greeting 객체 추가

---
+++ src/main/java/com/example/studyspringmessagingstompwebsocket/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.studyspringmessagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+    private String content;
+
+}
